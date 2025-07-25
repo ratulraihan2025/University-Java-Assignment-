358b68b046bdf44747cf8b1cebc7ce67189ae18f
Ratul Raihan <ratulraihan2025@gmail.com>
2025-07-25 15:11:46 +0600

    Create Series (1to100) print.java

---
+++ Series (1to100) print.java
@@ -0,0 +1,12 @@
+public class Series1to100 {
+   public static void main(String[] args) {
+    
+   int i;
+   System.out.println("1 to 100 :");
+   for(i=1;i<=100;i++)
+   {
+     System.out.print(i + " ");
+   }
+ } 
+
+}
