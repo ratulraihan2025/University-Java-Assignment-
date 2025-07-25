20f874865d25a7ab1143f785cff06d08447240ab
Ratul Raihan <ratulraihan2025@gmail.com>
2025-07-25 15:03:02 +0600

    Create Series-Even.java

---
+++ Series-Even.java
@@ -0,0 +1,14 @@
+public class SeriesOfEven {
+    public static void main(String[] args) {
+      int i;
+      System.out.println("Even numbers from 1 to 10 : "); 
+      
+    for(i = 1; i <= 100; i++)
+    {
+      if(i % 2 == 0)
+      {
+        System.out.println(i + "");
+      }
+    }
+  }
+}
